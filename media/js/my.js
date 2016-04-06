d45e4ed53e01e00626d4be93ac4a9465fb0d1b45
Helene <lenagritsuk@inbox.ru>
2016-04-06 19:16:06 +0200

    'pirozhnoe-nachalo'

--- media/js/my.js
+++ media/js/my.js
@@ -16,17 +16,28 @@ $('#header').css('background', 'url(media/img/topcurves.png) center top repeat-x
 
 });*/
 
-$(function(){
+var main = function () {
 
-$('.leftmenu a').bind('click',function() {
+/*$('.leftmenu a').bind('click',function() {
 data=$(this).attr('data-title');
 data2=$(this).attr('data-body');
 console.log(data);
-$('.task').text(data); 
- $('.task').append('<br>',data2);    
+$('.tel').html(data); 
+ $('.tel').append(data2);    
 
+});*/
+    
+$('.text').bind({'mouseenter' : function () {
+$(this).css('display', 'none');
+$('.knopka').css({'display':"block"});
+
+},
+ 'mouseleave':function() { 
+ $(this).css('display', 'block');    
+$('.knopka').css({'display':"none"});
+     
+} 
 });
-   
-
-
-});
+ 
+};
+$(document).ready(main);
